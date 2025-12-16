e78fbe42097efdc324ee3693d0d06ba8f8253566
KananDave22 <kanandave11@gmail.com>
2025-12-15 23:01:26 -0500

    Navbar Done

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './components/Nav/Nav'
+import Index from './components/Page'
 
 const App = () => {
   return (
     <div>
       <Nav />
+      <Index />
     </div>
   )
 }
--- notes-app/src/components/Nav/Nav.jsx
+++ notes-app/src/components/Nav/Nav.jsx
@@ -1,43 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 
 function Nav() {
-
-  const [darkMode, setDarkMode] = useState(() =>{
-    return localStorage.getItem('theme') === 'dark';
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
   });
 
   useEffect(() => {
     const root = document.documentElement;
 
-    if(darkMode){
-        root.classList.add('dark-mode');
-        root.classList.remove('light-mode');
-        localStorage.setItem('theme','dark');
+    if (darkMode) {
+      root.classList.add("dark-mode");
+      root.classList.remove("light-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.remove("dark-mode");
+      root.classList.add("light-mode");
+      localStorage.setItem("theme", "light");
     }
-    else{
-        root.classList.remove('dark-mode');
-        root.classList.add('light-mode');
-        localStorage.setItem('theme', 'light')
-    }
-  }, [darkMode]); 
-    
+  }, [darkMode]);
 
   const toggleDarkMode = () => {
-        setDarkMode(prev => !prev);
-  }
+    setDarkMode((prev) => !prev);
+  };
 
   return (
     <>
-        <nav className="navbar">
-            <div className="logo">
-                <h1>Your <span>Notes</span></h1>
-            </div>
-            <div className="nav-buttons">
-                <button className="btn"></button>
-            </div>
-        </nav>
+      <nav className="navbar">
+        <div className="logo">
+          <h1>
+            Your <span>Notes</span>
+          </h1>
+        </div>
+        <div className="nav-buttons">
+          <button className="btn">
+            <i className="ri-add-large-fill"></i>&nbsp; Add Note
+          </button>
+
+          <div className="toggle-switch">
+            <label className="switch-label">
+              <input
+                type="checkbox"
+                className="checkbox"
+                checked={darkMode}
+                onChange={toggleDarkMode}
+              />
+              <span className="slider"></span>
+            </label>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
